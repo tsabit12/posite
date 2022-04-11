a55c4c028b756c7018460715babb7a9ab491f720
TSABIT-ABDUL-at-005813420300 <TSABIT-ABDUL-at-005813420300>
2022-04-12 00:35:20 +0700

    add produk dropdown

--- src/layout/MinimalLayout/components/Navbar/Navbar.js
+++ src/layout/MinimalLayout/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import { AppBar, Box, Button, Stack, Toolbar, Typography } from '@mui/material';
-import React from 'react'
+import { alpha, AppBar, Box, Button, Chip, Menu, MenuItem, Stack, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react'
 
 const NavbarRoot = styled(AppBar)({
      backgroundColor: '#FFFFFF',
@@ -29,7 +29,65 @@ const ButtonLink = styled(Box)({
      alignItems: 'center'
 })
 
+const StyledMenu = styled((props) => (
+     <Menu
+       elevation={0}
+       anchorOrigin={{
+         vertical: 'bottom',
+         horizontal: 'right',
+       }}
+       transformOrigin={{
+         vertical: 'top',
+         horizontal: 'right',
+       }}
+       {...props}
+     />
+   ))(({ theme }) => ({
+     '& .MuiPaper-root': {
+          borderRadius: 6,
+          marginTop: theme.spacing(1),
+          minWidth: 300,
+          color: 'rgb(55, 65, 81)',
+          boxShadow:
+          'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+          '& .MuiMenu-list': {
+               padding: '4px 0',
+          },
+          '& .MuiMenuItem-root': {
+          '& .MuiSvgIcon-root': {
+               fontSize: 18,
+               color: theme.palette.text.secondary,
+               marginRight: theme.spacing(1.5),
+          },
+          '&:active': {
+               backgroundColor: alpha(
+                    theme.palette.primary.main,
+                    theme.palette.action.selectedOpacity,
+               ),
+          },
+       },
+     },
+}));
+
+const CustomChip = styled(Chip)({
+     backgroundColor: 'rgba(221, 32, 37, 0.2)',
+     color: '#DD2025',
+     fontSize: '10px',
+     fontWeight: '500',
+     lineHeight: '18.1px'
+})
+
 const Navbar = () => {
+     const [anchorEl, setAnchorEl] = useState(null);
+
+     const open = Boolean(anchorEl);
+     const handleClick = (event) => {
+          setAnchorEl(event.currentTarget);
+     };
+     const handleClose = () => {
+          setAnchorEl(null);
+     };
+
      return(
           <NavbarRoot 
                position="fixed" 
@@ -50,7 +108,12 @@ const Navbar = () => {
                     </GradientButton>
                     <Box sx={{ flexGrow: 1 }} />
                     <Stack direction={'row'} alignItems='center' spacing={'35px'}>
-                         <ButtonLink>
+                         <ButtonLink 
+                              onClick={handleClick}
+                              aria-controls={open ? 'dropdown-menu' : undefined}
+                              aria-haspopup="true"
+                              aria-expanded={open ? 'true' : undefined}
+                         >
                               <Typography variant='link'>Produk</Typography>
                               <img src={`${process.env.REACT_APP_PUBLIC_URL}/assets/icon/akar-icons_chevron-down.svg`} alt='produk' style={{ marginLeft: '8px'}} />
                          </ButtonLink>
@@ -60,6 +123,55 @@ const Navbar = () => {
                          <Button variant='contained' color='primary'>Kontribusi</Button>
                     </Stack>
                </Toolbar>
+
+               <StyledMenu
+                    id="framework-menu"
+                    MenuListProps={{
+                         'aria-labelledby': 'framework-menu',
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+               >
+                    <Stack spacing={'14px'}>
+                         <MenuItem>
+                              <Stack direction={'row'} spacing={'12px'} alignItems='center'>
+                                   <img alt='image1' src={`${process.env.REACT_APP_PUBLIC_URL}/assets/28.svg`} />
+                                   <Box>
+                                        <Typography variant='body2' color={'#081A51'}>Front End UI Library</Typography>
+                                        <Typography 
+                                             fontSize={'12px'} 
+                                             fontWeight='400' 
+                                             color={'rgba(37, 39, 63, 0.7)'}
+                                             lineHeight='181%'
+                                        >
+                                             Ada <span style={{ color: '#F68407', fontWeight: '700'}}>+200</span> library yang kami sediakan
+                                        </Typography>
+                                   </Box>
+                              </Stack>
+                         </MenuItem>
+
+                         <MenuItem>
+                              <Stack direction={'row'} spacing={'12px'} alignItems='center'>
+                                   <img alt='image1' src={`${process.env.REACT_APP_PUBLIC_URL}/assets/29.svg`} />
+                                   <Box>
+                                        <Typography variant='body2' color={'#081A51'}>UI Komponen</Typography>
+                                        <CustomChip label='😎 Coming Soon' />
+                                   </Box>
+                              </Stack>
+                         </MenuItem>
+
+                         <MenuItem>
+                              <Stack direction={'row'} spacing={'12px'} alignItems='center'>
+                                   <img alt='image1' src={`${process.env.REACT_APP_PUBLIC_URL}/assets/30.svg`} />
+                                   <Box>
+                                        <Typography variant='body2' color={'#081A51'}>Website Builder</Typography>
+                                        <CustomChip label='😎 Coming Soon' />
+                                   </Box>
+                              </Stack>
+                         </MenuItem>
+                    </Stack>
+               </StyledMenu>
           </NavbarRoot>
      )    
 }
