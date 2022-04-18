fdc1b43c72dff9641d91abbbc084a48628c6f1e8
TSABIT-ABDUL-at-005813420300 <TSABIT-ABDUL-at-005813420300>
2022-04-18 22:12:20 +0700

    redux configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { ThemeProvider } from "@mui/system";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import Routes from "./Routes";
 import theme from "./theme";
+import rootReducer from './rootReducer';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Routes />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import session from "./reducers/session";
+
+export default combineReducers({
+     session
+})
