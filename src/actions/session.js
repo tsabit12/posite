b484bd5d023e507edf4817191bd2a372bfedde15
tsabit12 <tsabit830@gmail.com>
2022-04-23 03:39:44 +0700

    add x-access-token after login

--- src/actions/session.js
+++ src/actions/session.js
@@ -1,8 +1,15 @@
 import { SET_LOGGED_IN } from "../types";
 import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 export const setLoggedIn = (token) => dispatch => {
      var { user } = jwt_decode(token);
+
+     axios.interceptors.request.use(function(config){
+          config.headers["x-access-token"] = token;
+          return config;
+     })
+
      dispatch({
           type: SET_LOGGED_IN,
           session: { token, ...user }
